96bd91a16b0a13b35c7e4480162c301fa4cc582e
harishbaranwal <114431658+harishbaranwal@users.noreply.github.com>
2023-10-14 10:46:39 +0530

    Create lecture12I.cpp

---
+++ lecture12I.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    cout << "the address of a is: "<< &a << endl;
+
+    float b;
+    cout << "the address of b is: "<< &b << endl;
+
+    
+
+    return 0;
+}
